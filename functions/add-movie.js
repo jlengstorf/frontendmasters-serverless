a7294d60b9f61f699d37704bc1acb09a25cad4ff
Jason Lengstorf <jason@lengstorf.com>
2020-07-22 12:33:46 -0700

    feat: access control implemented

--- functions/add-movie.js
+++ functions/add-movie.js
@@ -1,7 +1,17 @@
 const { query } = require('./util/hasura');
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
   const { id, title, tagline, poster } = JSON.parse(event.body);
+  const { user } = context.clientContext;
+  const isLoggedIn = user && user.app_metadata;
+  const roles = user.app_metadata.roles || [];
+
+  if (!isLoggedIn || !roles.includes('admin')) {
+    return {
+      statusCode: 401,
+      body: 'Unauthorized',
+    };
+  }
 
   const result = await query({
     query: `
